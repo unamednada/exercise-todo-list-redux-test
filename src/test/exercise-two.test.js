e19127faa7eb9839ad7236e97a191c3de378bb50
unamednada <dias.gustavo.dias@gmail.com>
2021-09-29 01:46:22 -0300

    Create test with foreach with listtodo exercise 2

--- src/test/exercise-two.test.js
+++ src/test/exercise-two.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import Item from '../Item';
@@ -7,8 +7,15 @@ import Item from '../Item';
 describe('Teste do campo de input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
-    const {} = render(<App />) // Caso precise de uma nova query adicione no object destructuring
-
+    render(<App />) // Caso precise de uma nova query adicione no object destructuring
+    const btnAdd = screen.getByRole('button', { name: /adicionar/i });
+    const input = screen.getByLabelText(/tarefa/i);
+    listTodo.forEach((item) => {
+      userEvent.type(input, item);
+      userEvent.click(btnAdd);
+    })
+    const todoList = screen.getAllByRole('listitem');
+    expect(todoList.length).toBe(3);
   })
 });
 
