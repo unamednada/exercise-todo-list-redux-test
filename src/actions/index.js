7499888f172b4002bd26b358393402d2d75d32ea
unamednada <dias.gustavo.dias@gmail.com>
2021-10-20 10:46:39 -0300

    Update initial state in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export const ADD_TODO = 'ADD_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
 
-export const addToDo = (todo) => ({ type: ADD_TODO, todo });
-export const removeToDo = (todo) => ({ type: REMOVE_TODO, todo });
+export const addToDo = (toDo) => ({ type: ADD_TODO, toDo });
+export const removeToDo = (toDo) => ({ type: REMOVE_TODO, toDo });
--- src/reducer/toDoReducer.js
+++ src/reducer/toDoReducer.js
@@ -1,15 +1,21 @@
 import { ADD_TODO, REMOVE_TODO } from "../actions";
 
-const INITIAL_STATE = [];
+const INITIAL_STATE = {
+  listToDo: [],
+  selected: null,
+};
 
 const toDoReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case ADD_TODO:
-      return [...state, action.todo];
+      return {
+        ...state,
+        listToDo: [...state.listToDo, action.toDo]
+      };
     case REMOVE_TODO:
       return [
-        ...state.split(0, state.indexOf(action.todo)),
-        ...state.split(state.indexOf(action.todo) + 1, state.length)
+        ...state.listToDo.split(0, state.listToDo.indexOf(action.toDo)),
+        ...state.listToDo.split(state.listToDo.indexOf(action.toDo) + 1, state.length)
       ];
     default:
       return state;
